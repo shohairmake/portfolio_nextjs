90acd3ae04b9cd524bf277ced5c7f33aa046a768
sho.tanaka <sho.tanaka@aijus.com>
2020-06-17 18:31:53 +0900

    test

---
+++ out/_next/static/webpack/static/development/pages/index.js.f9e83ee9f8015b22a3cc.hot-update.js
@@ -0,0 +1,113 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./public/static/album/hair1.png":
+/*!***************************************!*\
+  !*** ./public/static/album/hair1.png ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/hair1-3a733cd6a05755115dc2054f50507448.png";
+
+/***/ }),
+
+/***/ "./public/static/album/hair2.png":
+/*!***************************************!*\
+  !*** ./public/static/album/hair2.png ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/hair2-0a11f6cf65d87194cd4baad5432ecc68.png";
+
+/***/ }),
+
+/***/ "./public/static/album/hair3.png":
+/*!***************************************!*\
+  !*** ./public/static/album/hair3.png ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/hair3-604786635604912dc2e3405dacab4305.png";
+
+/***/ }),
+
+/***/ "./public/static/album/hair4.png":
+/*!***************************************!*\
+  !*** ./public/static/album/hair4.png ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/hair4-918dc72fe7fd4cc6fa914918b3d6af66.png";
+
+/***/ }),
+
+/***/ "./public/static/album/hair5.png":
+/*!***************************************!*\
+  !*** ./public/static/album/hair5.png ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/hair5-af6968cb3377fd8f03d2a76f1ab72e2b.png";
+
+/***/ }),
+
+/***/ "./src/components/Album/tileData.js":
+/*!******************************************!*\
+  !*** ./src/components/Album/tileData.js ***!
+  \******************************************/
+/*! exports provided: tileData */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "tileData", function() { return tileData; });
+/* harmony import */ var _public_static_album_hair1_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../public/static/album/hair1.png */ "./public/static/album/hair1.png");
+/* harmony import */ var _public_static_album_hair1_png__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_public_static_album_hair1_png__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _public_static_album_hair2_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../public/static/album/hair2.png */ "./public/static/album/hair2.png");
+/* harmony import */ var _public_static_album_hair2_png__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_public_static_album_hair2_png__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _public_static_album_hair3_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../public/static/album/hair3.png */ "./public/static/album/hair3.png");
+/* harmony import */ var _public_static_album_hair3_png__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_public_static_album_hair3_png__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _public_static_album_hair4_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../public/static/album/hair4.png */ "./public/static/album/hair4.png");
+/* harmony import */ var _public_static_album_hair4_png__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_public_static_album_hair4_png__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _public_static_album_hair5_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../../public/static/album/hair5.png */ "./public/static/album/hair5.png");
+/* harmony import */ var _public_static_album_hair5_png__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_public_static_album_hair5_png__WEBPACK_IMPORTED_MODULE_4__);
+
+
+
+
+
+var tileData = [{
+  img: _public_static_album_hair1_png__WEBPACK_IMPORTED_MODULE_0___default.a,
+  title: 'hair1',
+  author: 'author',
+  cols: 3
+}, {
+  img: _public_static_album_hair2_png__WEBPACK_IMPORTED_MODULE_1___default.a,
+  title: 'hair2',
+  author: 'author',
+  cols: 2
+}, {
+  img: _public_static_album_hair3_png__WEBPACK_IMPORTED_MODULE_2___default.a,
+  title: 'hair3',
+  author: 'author',
+  cols: 5
+}, {
+  img: _public_static_album_hair4_png__WEBPACK_IMPORTED_MODULE_3___default.a,
+  title: 'hair4',
+  author: 'author',
+  cols: 2
+}, {
+  img: _public_static_album_hair5_png__WEBPACK_IMPORTED_MODULE_4___default.a,
+  title: 'hair5',
+  author: 'author',
+  cols: 3
+}];
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.f9e83ee9f8015b22a3cc.hot-update.js.map
