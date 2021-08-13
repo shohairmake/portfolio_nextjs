fc964ed8f49bead21e96c67d95a99b8293d7613c
sho <scissors.core@gmail.com>
2021-08-13 18:52:00 +0900

    test

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,6 +1,6 @@
 // lib/gtag.ts
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || ''
-export const OPT_TRACKING_ID = process.env.NEXT_PUBLIC_OPT_ID || ''
+export const GA_TRACKING_ID = 'UA-120513195-2'
+export const OPT_TRACKING_ID = 'OPT-NKMN2C8'
 
 export const pageview = (url) => {
     window.gtag('config', GA_TRACKING_ID, {
